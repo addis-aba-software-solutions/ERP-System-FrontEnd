8a90e86e7efef21a7609e20dd660dc4dc0d99845
Yohannes Berhanu <Johnlights51@gmail.com>
2020-04-03 16:54:38 +0300

    After Login Customization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import HomeNavBar from './Dashboard/HomeNavBar';
-import Calendar from './Dashboard/Calendar';
+// import HomeNavBar from './Dashboard/HomeNavBar';
+// import Calendar from './Dashboard/Calendar';
 import Login from './Login/Login'
 function App() {
   return (
