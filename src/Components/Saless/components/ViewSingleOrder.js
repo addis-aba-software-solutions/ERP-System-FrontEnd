c962913ebf0e010a98f93fc12cc241d3dd60547d
Yohannes Berhanu <Johnlights51@gmail.com>
2020-04-11 15:28:25 +0300

    Finished with  Create and View Orders, Order Creation: FINANCE--SALES--LOGISTICS

--- src/Components/Saless/components/ViewSingleOrder.js
+++ src/Components/Saless/components/ViewSingleOrder.js
@@ -4,7 +4,7 @@ class ViewSingleOrder extends React.Component {
     render() {
         return(
             <div>
-              <h1> View ViewSingleOrder </h1>  
+              <h1> View ViewSingleOrder </h1> 
             </div>
         )
     }
